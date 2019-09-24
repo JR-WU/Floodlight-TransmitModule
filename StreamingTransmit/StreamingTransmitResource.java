d85ed7df65ff336e85618fb5904a9dba38215f93
Vinecent_Wu <wjr930914@gmail.com>
2019-09-24 09:18:26 +0800

    Update StreamingTransmitResource.java

--- StreamingTransmit/StreamingTransmitResource.java
+++ StreamingTransmit/StreamingTransmitResource.java
@@ -5,15 +5,19 @@ import org.restlet.resource.ServerResource;
 import java.io.IOException;
 
 import org.restlet.resource.Post;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StreamingTransmitResource extends ServerResource{
 	
 	protected String IPSrc;
 	protected String IPDst;
+	protected Logger logger = LoggerFactory.getLogger(StreamingTransmitResource.class);//output logger
 	
 	//handle external post from others and phrase it.
 	@Post
 	public String handlePost(String fromexternal) throws IOException{
+		
 		
 		return null;
 		
