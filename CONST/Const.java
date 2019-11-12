9759441fbbaf92594453b22ca31f513f4fb0404a
wbsuser <sunwb_mai@163.com>
2019-11-12 21:43:06 +0800

    null

--- CONST/Const.java
+++
@@ -1,6 +0,0 @@
-package net.floodlightcontroller.CONST;
-
-public class Const {
-	public static final String STREAM_SERVER_IP = "192.168.3.102";
-	public static final int STREAM_SERVER_PORT = 54001;
-}
--- CvTest/src/main/java/Const.java
+++
@@ -1,6 +0,0 @@
-public class Const {
-    public static final String STREAM_SERVER_IP = "192.168.3.102";
-    public static final int STREAM_SERVER_PORT = 54001; //UDP port
-    public static final int RTSP_PORT = 12345;
-    public static final int DATA_LEN = 1024;
-}
